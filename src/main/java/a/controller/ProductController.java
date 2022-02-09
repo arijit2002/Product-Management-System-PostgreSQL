6f969f27b0b1ddd05dea81a62e2f42fd00e4efbc
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-02-09 23:08:25 +0530

    Update ProductController.java

--- src/main/java/a/controller/ProductController.java
+++ src/main/java/a/controller/ProductController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import a.model.Product;
 import a.service.ProductService;
 
