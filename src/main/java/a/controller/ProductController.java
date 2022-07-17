67b63ee5d7484fb041f6808b4520c8d9c9ddda25
Arijit Das <arijit10das2002@gmail.com>
2022-07-17 10:43:38 +0530

    sec up

--- src/main/java/a/controller/ProductController.java
+++ src/main/java/a/controller/ProductController.java
@@ -23,28 +23,33 @@ public class ProductController {
 		m.addAttribute("listProducts", productService.getAllProducts());
 		return "index";
 	}
+	
 	@GetMapping("/showNewProductForm")
 	public String showNewProductform( Model m) {
 		Product product=new Product();
 		m.addAttribute("product",product);
 		return "new_product";
 	}
+	
 	@PostMapping("/saveProduct")
 	public String saveProduct(@ModelAttribute("product") Product product) {
 		productService.saveProduct(product);
 		return "redirect:/";
 	}
+	
 	@GetMapping("/showFormUpdate/{id}")
 	public String showFormUpdate(@PathVariable(value = "id") long id,Model model) {
 		Product product = productService.getProductById(id);
 		model.addAttribute("product", product);
 		return "update_product";
 	}
+	
 	@GetMapping("/deleteProduct/{id}")
 	public String deleteProduct(@PathVariable(value="id") long id) {
 		this.productService.deleteById(id);
 		return "redirect:/";
 	}
+	
 	@GetMapping("/admin")
 	public String admin(@AuthenticationPrincipal User user) {
 		return "redirect:/";
